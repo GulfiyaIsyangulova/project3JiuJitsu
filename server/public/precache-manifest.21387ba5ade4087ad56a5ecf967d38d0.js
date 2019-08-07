258039e476aa2613f3b8686b89b6edb585d3a085
Gulfiya Isyangulova <gulfiyaisyangulova@Gulfiyas-MacBook-Pro.local>
2019-08-07 15:58:18 -0400

    deploying

---
+++ server/public/precache-manifest.21387ba5ade4087ad56a5ecf967d38d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbb4b02724ab284f9069c0c99f8ddff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b8e11129c7c3b0d5f2e",
+    "url": "/static/css/main.77809d3a.chunk.css"
+  },
+  {
+    "revision": "3977e51a585524ef3fe4",
+    "url": "/static/js/2.f97c190c.chunk.js"
+  },
+  {
+    "revision": "2b8e11129c7c3b0d5f2e",
+    "url": "/static/js/main.e4a39cb8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
